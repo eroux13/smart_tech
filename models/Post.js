4496bd7697cae4b648596f995686a55d74bd714c
EJ <ej.roux13@gmail.com>
2021-04-21 18:59:46 -0700

    finished building Post model

--- models/Post.js
+++ models/Post.js
@@ -30,6 +30,7 @@ Post.init(
         },
         user_id: {
             type: DataTypes.INTEGER,
+            allowNull: false,
             references: {
                 model: "user",
                 key: "id"
