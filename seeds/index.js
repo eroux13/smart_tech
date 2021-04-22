4a7a0cb399e902b190f0aa2edf5142b5034c2888
EJ <ej.roux13@gmail.com>
2021-04-22 03:51:54 -0700

    created seeds

---
+++ seeds/index.js
@@ -0,0 +1,28 @@
+// Import Seeds
+const userSeeds = require("./user-seeds");
+const postSeeds = require("./post-seeds");
+const commentSeeds = require("./comment-seeds");
+
+// Import Sequelize connection
+const sequelize = require("./../config/connection");
+const chalk = require("chalk");
+const { create } = require("../models/User");
+
+// Create seeds
+const createSeeds = () => {
+    await sequelize.sync({force: true});
+    console.log(chalk.bold.green("\n----- DATABASE SYNCED -----\n"));
+
+    await userSeeds();
+    console.log(chalk.bold.green("\n----- USERS SEEDED -----\n"));
+
+    await postSeeds();
+    console.log(chalk.bold.green("\n----- POSTS SEEDED -----\n"));
+    
+    await commentSeeds();
+    console.log(chalk.bold.green("\n----- COMMENTS SEEDED -----\n"));
+
+    process.exit(0);
+}
+
+createSeeds();
