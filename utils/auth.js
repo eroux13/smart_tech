0c642cc09dd10f834439154b903bba4858a7765a
EJ <ej.roux13@gmail.com>
2021-05-01 09:18:06 -0700

    added functionality for authentication helper

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+// Custom Authentication Helper
+const withAuth = (req, res, next) => {
+    if (!req.session.user_id){
+        res.redirect("/login");
+    }
+    else {
+        next();
+    }
+};
+
+module.exports = withAuth;
