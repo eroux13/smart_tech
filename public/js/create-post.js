9a3378806580dc59f6624477db32c1be6891a98d
EJ <ej.roux13@gmail.com>
2021-05-01 06:01:22 -0700

    added functionality for creating a new post

---
+++ public/js/create-post.js
@@ -0,0 +1,17 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+    const title = document.querySelector("input[name='post-title']").value;
+    const post = document.querySelector("textarea[name='post-content']").value;
+    const response = await fetch("/api/posts", {
+        method: "POST",
+        body: JSON.stringify({title, post}),
+        headers: {"Content-Type": "application/json"}
+    });
+    if (response.ok){
+        document.location.replace("/dashboard");
+    }
+    else {
+        alert(response.statusText);
+    }
+}
+document.querySelector(".new-post-form").addEventListener("submit", newFormHandler);
