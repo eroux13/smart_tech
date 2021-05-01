3894b8c938ffb377ab5eedaa4a405c57f38f3141
EJ <ej.roux13@gmail.com>
2021-05-01 04:59:04 -0700

    finished GET routes for posts

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,90 @@
+// Import Dependencies
+const router = require("express").Router();
+const {Post, User, Comment} = require("./../../models");
+const withAuth = require("./../../utils/auth");
+const sequelize = require("./../../config/connection");
+
+// GET api/posts
+router.get("/", async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            attributes: [
+                "id",
+                "post_content",
+                "title",
+                "created_at"
+            ],
+            order: [["created_at", "DESC"]],
+            include: [
+                {
+                    model: User,
+                    attributes: ["user_name"]
+                },
+                {
+                    model: Comment,
+                    attributes: [
+                        "id",
+                        "comment_text",
+                        "post_id",
+                        "user_id",
+                        "created_at"
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ["user_name"]
+                    }
+                }
+            ]
+        })
+
+        res.json(postData);
+
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// GET api/posts/:id
+router.get("/:id", async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id, {
+            attributes: [
+                "id",
+                "post_content",
+                "title",
+                "created_at"
+            ],
+            include: [
+                {
+                    model: User,
+                    attributes: ["user_name"]
+                },
+                {
+                    model: Comment,
+                    attributes: [
+                        "id",
+                        "comment_text",
+                        "post_id",
+                        "user_id",
+                        "created_at"
+                    ],
+                    include: {
+                        model: User,
+                        attributes: ["user_name"]
+                    }
+                }
+            ]
+        })
+        if (!postData) {
+            res.status(404).json({message: "No post found!"});
+            return;
+        }
+
+        res.json(postData);
+
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+});
