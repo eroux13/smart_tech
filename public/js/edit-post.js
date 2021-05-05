f608ab1686915e73d7a55cf0c078b336061f9099
EJ <ej.roux13@gmail.com>
2021-05-04 23:16:02 -0700

    updated reference to post_content

--- public/js/edit-post.js
+++ public/js/edit-post.js
@@ -1,13 +1,13 @@
-const editFormHandler = (event) => {
-    event.preventDefualt();
+const editFormHandler = async (event) => {
+    event.preventDefault();
     const id = window.location.toString().split("/")[
         window.location.toString().split("/").length - 1
     ];
     const title = document.querySelector("input[name='post-title']").value;
-    const post = document.querySelector("textarea[name='post-content']").value;
+    const post_content = document.querySelector("textarea[name='post-content']").value;
     const response = await fetch(`/api/posts/${id}`, {
         method: "PUT",
-        body: JSON.stringify({title, post}),
+        body: JSON.stringify({title, post_content}),
         headers: {"Content-Type": "application/json"}
     });
     if (response.ok){
